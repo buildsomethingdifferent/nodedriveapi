b47a79c83822f2057539b893f258529e89790830
Awais <mmuhammadaawais@gmail.com>
2020-02-15 19:48:56 +0500

    remove file

--- config/keys.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    google:{
-        clientID:'712624256632-3isg017dcejnshjfaa26aoqr8dg1isni.apps.googleusercontent.com',
-        clientSecret:'_tpizzjLn0OX8kO8tx0D2dAv',
-        callBack:'http://localhost:3000/auth/google-drive/callback'
-    }
-}
